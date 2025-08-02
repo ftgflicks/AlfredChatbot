5d9ad0d033e4a34294b1d00b6311795e1b7a0f51
ftgflicks <133413248+ftgflicks@users.noreply.github.com>
2025-08-02 02:51:44 -0700

    Update alfred_app.py

--- alfred_app.py
+++ alfred_app.py
@@ -16,7 +16,8 @@ st.markdown("""
         opacity: 0.85;
     }
     </style>
-    <img class="bat-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Batman_Logo.svg/2048px-Batman_Logo.svg.png">
+    <img class="bat-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Batman_Logo.svg/2048px-Batman_Logo.svg.png
+">
 """, unsafe_allow_html=True)
 
 # Set page config
